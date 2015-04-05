a5df41b07d1f67bff6fa3ccb775128886a69722a
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-04-04 22:23:06 -0700

    Updated to take advantage of new library features

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"os"
 )
 
-var category = flag.String("category", fakku.Manga, "the type of the content")
+var category = flag.String("category", fakku.CategoryManga, "the type of the content")
 var name = flag.String("name", "", "the name of the content itself, this is usually what you would find in the URL")
 
 func main() {
@@ -52,13 +52,8 @@ func main() {
 		log.Fatal(err1)
 	}
 	for ind, page := range pages {
-		purl, perr := page.ImageUrl()
-		if perr != nil {
-			log.Fatal(perr)
-		}
-		dfErr := DownloadFile(purl, fmt.Sprintf("%03d.jpg", ind), 0644)
-		if dfErr != nil {
-			log.Print(dfErr)
+		if dfErr := page.SaveImage(fmt.Sprintf("%03d.jpg", ind), 0644); dfErr != nil {
+			log.Fatal(dfErr)
 		}
 	}
 }
