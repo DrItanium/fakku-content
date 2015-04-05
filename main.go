bff3dc4e6e8a93850b8def54b0b2c990f636c84c
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-04-04 23:35:46 -0700

    Fixed up the tool to use tabwriter
    - Added more options to control different aspects

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"github.com/DrItanium/fakku"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
+	"text/tabwriter"
 )
 
 var category = flag.String("category", fakku.CategoryManga, "the type of the content")
 var name = flag.String("name", "", "the name of the content itself, this is usually what you would find in the URL")
+var downloadImages = flag.Bool("download", false, "Download the content you would read online to the current working directory")
+var comments = flag.Bool("comments", false, "Show the content's comments")
 
 func main() {
 	flag.Parse()
@@ -26,52 +26,39 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Name:", content.Name)
-	covUrl, errcu := content.CoverUrl()
-	if errcu != nil {
-		log.Fatal(errcu)
-	}
-	fmt.Println("Cover URL:", covUrl.String())
-	comments, errcm := content.Comments()
-	if errcm != nil {
-		log.Fatal(errcm)
-	}
-	fmt.Println("Comments")
-	for _, comment := range comments.Comments {
-		fmt.Printf("\t[%s] %s - %s\n", comment.Date(), comment.Poster, comment.Text)
-	}
+	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
+	fmt.Fprintf(tw, "Type:\t%s\n", *category)
+	fmt.Fprintf(tw, "Name:\t%s\n", content.Name)
+	fmt.Fprintf(tw, "Artists:\t%s\n", content.ArtistsString())
+	fmt.Fprintf(tw, "Series:\t%s\n", content.SeriesString())
+	fmt.Fprintf(tw, "Translators:\t%s\n", content.TranslatorsString())
+	fmt.Fprintf(tw, "Tags:\t%s\n", content.TagsString())
+	tw.Flush()
 
-	// try getting the covUrl's contents
-	derr0 := DownloadFile(covUrl, "cover-thumbnail.jpg", 0644)
-	if derr0 != nil {
-		log.Fatal(derr0)
-	}
-	// dump the read-online stuff
-	pages, err1 := content.ReadOnline()
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-	for ind, page := range pages {
-		if dfErr := page.SaveImage(fmt.Sprintf("%03d.jpg", ind), 0644); dfErr != nil {
-			log.Fatal(dfErr)
+	if *comments {
+		comments, errcm := content.Comments()
+		if errcm != nil {
+			log.Fatal(errcm)
+		}
+		fmt.Println("Comments")
+		for _, comment := range comments.Comments {
+			fmt.Printf("\t[%s] %s - %s\n", comment.Date(), comment.Poster, comment.Text)
 		}
 	}
-}
 
-func DownloadFile(url *url.URL, outputDir string, perms os.FileMode) error {
-	resp, rerr := http.Get(url.String())
-	if rerr != nil {
-		return rerr
+	if *downloadImages {
+		if derr0 := content.SaveCover("cover-thumbnail.jpg", 0644); derr0 != nil {
+			log.Fatal(derr0)
+		}
+		// dump the read-online stuff
+		pages, err1 := content.ReadOnline()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		for ind, page := range pages {
+			if dfErr := page.SaveImage(fmt.Sprintf("%03d.jpg", ind), 0644); dfErr != nil {
+				log.Fatal(dfErr)
+			}
+		}
 	}
-	defer resp.Body.Close()
-	img, ierr := ioutil.ReadAll(resp.Body)
-
-	if ierr != nil {
-		return ierr
-	}
-	werr := ioutil.WriteFile(outputDir, img, perms)
-	if werr != nil {
-		return werr
-	}
-	return nil
 }
